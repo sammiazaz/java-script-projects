bf117b4cca4de5a3530ebd959c0f0c42f5e0e2ff
sammiazaz <SAMMIAZAZ2004@GMAIL.COM>
2026-04-02 13:23:11 +0530

    passing array in function

--- 19_functions/function.js
+++ 19_functions/function.js
@@ -17,8 +17,28 @@
 
 
 // rest operator
-function calculateCartPrice(...item1){
-    return item1
+// function calculateCartPrice(...item1){
+//     return item1
+// }
+
+// console.log(calculateCartPrice(500, 700, 800))
+
+//-----------passing an object in function
+
+const user = {
+    username : "sammi azaz",
+    age : 22
+}
+function objectinfunction(object){
+    console.log(`username is ${object.username} and age is ${object.age}`)
 }
 
-console.log(calculateCartPrice(500, 700, 800))
+objectinfunction(user)
+
+//-------------passing array in object-------
+const arr = [40, 60, 500]
+
+function ArrayinFunction(array){
+    return array[0]
+}
+console.log(ArrayinFunction(arr));
---
+++ 19_functions/tempCodeRunnerFile.js
@@ -0,0 +1,5 @@
+function calculateCartPrice(...item1){
+//     return item1
+// }
+
+// console.log(calculateCartPrice(500, 700, 800))
