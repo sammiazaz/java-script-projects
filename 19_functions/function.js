4ca6136ceb1b520b7eb49fe85fa0d171328806fe
sammiazaz <SAMMIAZAZ2004@GMAIL.COM>
2026-04-02 13:04:51 +0530

    rest operator

--- 19_functions/function.js
+++ 19_functions/function.js
@@ -1,16 +1,24 @@
-function sum (a,b){
-    let add = (a+b)
-    return add
+// function sum (a,b){
+//     let add = (a+b)
+//     return add
+// }
+
+// console.log(sum(5,8));
+
+// // normal function 
+// function add(a, b) {
+//     return a + b;
+// }
+
+// // arrow function 
+// const add = (a, b) => {
+//     return a + b;
+// };
+
+
+// rest operator
+function calculateCartPrice(...item1){
+    return item1
 }
 
-console.log(sum(5,8));
-
-// normal function 
-function add(a, b) {
-    return a + b;
-}
-
-// arrow function 
-const add = (a, b) => {
-    return a + b;
-};
+console.log(calculateCartPrice(500, 700, 800))
