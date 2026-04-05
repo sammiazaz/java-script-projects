1068d08c5701a90ceb8d6c673c5df1c9af6754f3
sammiazaz <SAMMIAZAZ2004@GMAIL.COM>
2026-04-05 17:12:13 +0530

    ope

--- 19_functions/scope.js
+++ 19_functions/scope.js
@@ -1,1 +1,24 @@
-// scope 
+// // scope let, var, const
+// let a = 10
+// const b = 20
+// var c = 30
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+
+
+// scope level and mini hosting ------------------
+function one(){
+    const username = "sammi"
+
+    function two(){
+        const web = "youtube"
+        console.log(web);
+        
+    }
+    two()
+    
+}
+one()
