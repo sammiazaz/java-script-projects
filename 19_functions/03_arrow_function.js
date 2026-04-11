6e648472a42e64a5c6fcc0c0aff8ea3aa03ca9ec
sammiazaz <SAMMIAZAZ2004@GMAIL.COM>
2026-04-11 20:19:29 +0530

    arrowfunction

--- 19_functions/03_arrow_function.js
+++ 19_functions/03_arrow_function.js
@@ -1,1 +1,14 @@
 // this and arrow function
+// const addtwo = (num1, num2) =>{
+//     return num1 + num2;
+// }
+
+// console.log( addtwo(5, 8));
+
+
+// implicit statement
+const addtwo = (num1, num2) => num1 + num2;
+console.log(addtwo(5,5));
+
+
+
