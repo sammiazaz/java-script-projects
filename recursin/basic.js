2417550e0f159e90603f0dccbc351a25664c817a
sammiazaz <SAMMIAZAZ2004@GMAIL.COM>
2026-04-18 23:45:11 +0530

    recusion

---
+++ recursin/basic.js
@@ -0,0 +1,16 @@
+function factorial(n) {
+  if (n <= 1) {
+    return 1;
+  }
+  return n * factorial(n - 1);
+}
+
+function fibonacci(n) {
+  if (n <= 1) {
+    return n;
+  }
+  return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+console.log("factorial(5) =", factorial(5));
+console.log("fibonacci(7) =", fibonacci(7));
